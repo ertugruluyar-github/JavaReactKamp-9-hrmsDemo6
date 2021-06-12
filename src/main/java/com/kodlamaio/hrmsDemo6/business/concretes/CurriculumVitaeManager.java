85c737a435ef22cc89667a2a3813da0e7f6011b0
euyar42 <ertugrul.uyar42@gmail.com>
2021-06-12 09:01:29 +0300

    cl

--- src/main/java/com/kodlamaio/hrmsDemo6/business/concretes/CurriculumVitaeManager.java
+++ src/main/java/com/kodlamaio/hrmsDemo6/business/concretes/CurriculumVitaeManager.java
@@ -74,7 +74,7 @@ public class CurriculumVitaeManager implements CurriculumVitaeService {
 		Object object = this.cloudinaryUploadService.upload(file).get("secure_url");
 		if ((object == null)) {
 			return new ErrorDataResult<String>("Failed to load photo! Not found image.",
-					String.valueOf(new File(".").getAbsoluteFile() + "    " + file.getAbsolutePath()));
+					String.valueOf(file.toURI() + "    " + file.getAbsolutePath()));
 		} else if (!this.curriculumVitaeDao.existsById(id)) {
 			return new ErrorDataResult<String>("Failed to load photo! Not found curriculum vitae.", null);
 		} else {
